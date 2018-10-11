5c9de38f03b5977c66ec791898e8e6db9b09fd0f
刘爽 <liushuang@liushuangdeMacBook-Pro.local>
2018-10-11 17:26:21 +0800

    docs

--- js/two-sum.js
+++ js/two-sum.js
@@ -1,12 +1,19 @@
+/*
+  为了对运行时间复杂度进行优化，我们需要一种更有效的方法来检查数组中是否存在目标元素。
+  如果存在，我们需要找出它的索引。保持数组中的每个元素与其索引相互对应的最好方法是什么？
+  哈希表。
+*/
+
 var twoSum = function (nums, target) {
-  let map = {}
-  let len = nums.length
+  const map = {}
+  const len = nums.length
 
   for (let i = 0; i < len; i++) {
-    let c = target - nums[i]
+    const c = target - nums[i]
+    // map[c]为满足条件的值
     if (map[c] !== undefined && map[c] !== i) {
       return [map[c], i]
     }
     map[nums[i]] = i
   }
-}
+};
