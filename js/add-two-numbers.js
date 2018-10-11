488f9c40cb8912a8dcfcd9ceb086d4a5b78954c3
刘爽 <liushuang@liushuangdeMacBook-Pro.local>
2018-10-11 18:05:53 +0800

    refactor

--- js/add-two-numbers.js
+++ js/add-two-numbers.js
@@ -1,4 +1,4 @@
-function ListNode (val) {
+function ListNode(val) {
   this.val = val
   this.next = null
 }
@@ -8,33 +8,30 @@ function ListNode (val) {
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-  let Head = new ListNode(0)
+  const Head = new ListNode(0)
   let curr = Head
   // 代表进位
-  let carry = 0
+  let carray = 0
 
-  while (l1 != null || l2 != null) {
-    let val1 = l1 != null ? l1.val : 0
-    let val2 = l2 != null ? l2.val : 0
-    let s = val1 + val2 + carry
-    // 大于9则进1位
+  while (l1 || l2) {
+    const val1 = l1 ? l1.val : 0
+    const val2 = l2 ? l2.val : 0
+    const s = val1 + val2 + carray
     if (s > 9) {
       curr.next = new ListNode(s - 10)
-      carry = 1
+      carray = 1
     } else {
       curr.next = new ListNode(s)
-      carry = 0
+      carray = 0
     }
     curr = curr.next
-
     if (l1) l1 = l1.next
     if (l2) l2 = l2.next
   }
 
-  // 最高位进一位
-  if (carry) {
+  if (carray) {
     curr.next = new ListNode(1)
   }
 
   return Head.next
-}
+};
